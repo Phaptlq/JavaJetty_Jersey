c62538cd1da3cfaa309fdadb88d66a506dff4b8e
Trương Lê Quang Pháp <51302869@hcmut.edu.vn>
2018-12-05 16:42:09 +0700

    - change filter

--- src/main/java/service/App.java
+++ src/main/java/service/App.java
@@ -1,8 +1,10 @@
 package service;
 import com.sun.jersey.spi.container.servlet.*;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.DoSFilter;
 
 import javax.servlet.DispatcherType;
 import java.util.EnumSet;
@@ -22,6 +24,15 @@ public class App {
                 "com.sun.jersey.config.property.packages",
                 "service");
         //TODO
+        final EnumSet<DispatcherType> REQUEST_SCOPE = EnumSet.of(DispatcherType.REQUEST);
+        // For limit
+        FilterHolder filterHolder = new FilterHolder( DoSFilter.class );
+        filterHolder.setInitParameter("maxRequestsPerSec", "1");  // max requests per second per client
+        filterHolder.setInitParameter("delayMs", "-1");           // millisec to delay excess requests. -1 means reject (for testing)
+        filterHolder.setInitParameter("remotePort", "false");     // true = track connections by remote ip+port
+        filterHolder.setInitParameter("enabled", "true");
+        filterHolder.setInitParameter("trackSessions", "true");
+        context.addFilter( filterHolder, "/*", REQUEST_SCOPE );
         servletHolder.setInitOrder(1);
         context.addServlet(servletHolder, "/*");
         context.addFilter(LimitFilter.class,"/*", EnumSet.of(DispatcherType.INCLUDE,DispatcherType.REQUEST));
