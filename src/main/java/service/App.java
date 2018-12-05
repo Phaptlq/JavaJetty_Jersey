b37ab1e51b5f505f8df9cb2c1b0b74fe01cf221f
Trương Lê Quang Pháp <51302869@hcmut.edu.vn>
2018-12-05 11:58:54 +0700

    - add

--- src/main/java/service/App.java
+++ src/main/java/service/App.java
@@ -6,7 +6,8 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 public class App {
     public static void main(String[] args) throws Exception{
-        Server server = new Server(8080);
+        String port = System.getenv("PORT") != null ? System.getenv("PORT") : "8080";
+        Server server = new Server(Integer.parseInt(port));
         ServletContextHandler context =
                 new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
         ServletHolder servletHolder =
